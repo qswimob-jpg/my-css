5ecc8e3c02c91aa8265f091bae2a13290c395df7
qswimob-jpg <qswimob@gmail.com>
2026-07-21 22:15:40 +0900

    Add files via upload

--- bst-echo.js
+++ bst-echo.js
@@ -1,3 +1,5 @@
-document.querySelectorAll('.bst-echo-header, .bst-echo-body, .bst-echo-mask-border').forEach(function(el) {
-  el.style.setProperty('background-color', '#ffffff', 'important');
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelectorAll('.bst-echo-header, .bst-echo-body, .bst-echo-mask-border').forEach(function(el) {
+    el.style.setProperty('background-color', '#ffffff', 'important');
+  });
 });
